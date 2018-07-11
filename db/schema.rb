c00b04588b64474272d407fae0cba3fade932ca9
fion chen <wakinglife_615@yahoo.com>
2018-07-12 02:09:57 +0800

    rails g favorite and update favorite.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711175111) do
+ActiveRecord::Schema.define(version: 20180711180155) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
