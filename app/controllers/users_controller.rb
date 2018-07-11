35687fab0c2b654793ec845ad6dc99337c454669
fion chen <wakinglife_615@yahoo.com>
2018-07-11 19:55:29 +0800

    add user controller action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+      def show
+
+      end
+
+      def edit
+        unless @user == current_user
+         redirect_to user_path(@user)
+        end
+      end
+
+      def update
+       if @user.update(user_params)
+         flash[:notice] = "User information was successfully updated"
+         redirect_to user_path(@user)
+       else
+         flash.now[:alert] = "User information was failed to update"
+         render :action => :edit
+       end
+      end
+
+
+  private
+
+     def set_user
+      @user = User.find(params[:id])
+     end
+
+     def user_params
+       params.require(:user).permit(:name, :intro, :passowrd)
+     end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "posts#index"
+  root "users#index"
+
+
+  resources :users
+
 end
