145314b4e05f1fb894c90fa8cbe994fd506adc43
fion chen <wakinglife_615@yahoo.com>
2018-07-12 05:49:51 +0800

    add favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180714115736) do
+ActiveRecord::Schema.define(version: 20180714115737) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20180714115736) do
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "question_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
