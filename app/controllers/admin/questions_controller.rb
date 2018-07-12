462eb72b4651b111dabca2904c988d3931b57ab0
Riemann <dr.mactt@gmail.com>
2018-07-13 00:51:14 +0800

    Add #index and #destroy to admin::questions_controller

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,2 +1,15 @@
 class Admin::QuestionsController < ApplicationController
+  def index
+    @questions = Question.page(params[:page]).per(7)
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    if @question.destroy
+      redirect_to admin_questions_path, notice: "Question has been deleted"
+    else
+      flash[:alert] = @question.errors.full_messages.to_sentence
+      redirect_to admin_questions_path
+    end
+  end
 end
