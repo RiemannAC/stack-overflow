3326314a0d9e791ccb414a4f97d9079399d6b400
Duncan <loxa10077@gmail.com>
2018-07-14 19:58:20 +0800

    g answer model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
+  factory :answer do
+    user nil
+    question nil
+    content "MyString"
+  end
   factory :user do
     email FFaker::Internet.email
     password '123456'
