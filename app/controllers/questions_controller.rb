8ea649afe0b2dccf7b3c445f6d3955613538335e
Duncan <loxa10077@gmail.com>
2018-07-14 13:05:47 +0800

    user can create question on question index page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,22 @@
 class QuestionsController < ApplicationController
   def index
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+
+    if @question.save
+      flash[:notice] = "Question created"
+    else
+      flash[:alert] = @question.errors.full_messages.to_sentence
+    end
+    redirect_back(fallback_location: questions_path)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
   end
 end
