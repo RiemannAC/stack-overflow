70e84973279568745d2fa458e301f2edc7aaa28a
Duncan <loxa10077@gmail.com>
2018-07-15 13:15:04 +0800

    setup relation between question_upvote and user and question

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :question_upvotes, dependent: :destroy
 
   def admin?
     self.role == "admin"
