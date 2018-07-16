02b5c48806a0db238227132ecb484a51ef3b291b
Riemann <dr.mactt@gmail.com>
2018-07-16 08:21:08 +0800

    Add is_favorited? method to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,8 @@ class Question < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
 end
